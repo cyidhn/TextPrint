c00f50db6c10ace4a911da9a2f1c89a3ee39b851
Jérémy DEMANGE <j@lachainemedias.com>
2020-02-27 01:57:05 +0100

    Ajout de la connexion

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <v-app>
     <v-container fluid>
-      <v-row no-gutters>
-        <Dialogs />
-        <v-col lg="2">
-          <Menu />
-        </v-col>
-        <v-col lg="10">
-          <Tabs />
-        </v-col>
-      </v-row>
+      <!-- Snackbar -->
+      <v-snackbar v-model="barinfo" :bottom="true" color="error" :timeout="3000">
+        Le login ou le mot de passe est incorrect.
+        <v-btn dark text @click="snackbarSupprimer = false">Fermer</v-btn>
+      </v-snackbar>
+      <!-- /Snackbar -->
+      <div v-if="connecte">
+        <v-row no-gutters>
+          <Dialogs />
+          <v-col lg="2">
+            <Menu />
+          </v-col>
+          <v-col lg="10">
+            <Tabs />
+          </v-col>
+        </v-row>
+      </div>
+      <div v-else>
+        <Connexion :login="login" :mdp="mdp" :connecte="connecte" :barinfo="barinfo" />
+      </div>
     </v-container>
   </v-app>
 </template>
@@ -18,6 +29,7 @@
 import Tabs from "./components/Tabs";
 import Menu from "./components/Menu";
 import Dialogs from "./components/Dialogs";
+import Connexion from "./components/Connexion";
 // import CreateProfil from "./components/dialogs/CreateProfilConnu";
 
 export default {
@@ -26,11 +38,15 @@ export default {
   components: {
     Tabs,
     Menu,
-    Dialogs
+    Dialogs,
+    Connexion
   },
 
   data: () => ({
-    //
+    login: "",
+    mdp: "",
+    connecte: true,
+    barinfo: false
   })
 };
 </script>
---
+++ front/src/components/Connexion.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <v-col class="justify-center">
+      <h1>TextPrint V.1.02</h1>
+      <v-form ref="form">
+        <v-text-field v-model="login" label="Login" required></v-text-field>
+        <v-text-field type="password" v-model="mdp" label="Mot de passe" required></v-text-field>
+        <v-btn color="success" class="mr-4" @click="connexion">Se connecter</v-btn>
+      </v-form>
+    </v-col>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    login: String,
+    mdp: String,
+    connecte: Boolean,
+    barinfo: Boolean
+  },
+
+  methods: {
+    connexion() {
+      if (
+        (this.login === "idhn" && this.mdp === "idhn3") ||
+        (this.login === "alexandra" && this.mdp === "idhn_chemiirita")
+      ) {
+        this.connecte = true;
+      } else {
+        this.barinfo = true;
+      }
+    }
+  }
+};
+</script>
