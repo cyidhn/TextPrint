26192067d787bba2139e07f7cebf1cd29c3ccd09
Jérémy DEMANGE <j@lachainemedias.com>
2020-03-03 02:18:06 +0100

    Modification dans les textes

--- front/src/components/windows/Texte.vue
+++ front/src/components/windows/Texte.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
     <v-container>
+      <v-row>
+        <v-col cols="12">
+          <v-btn
+            @click="removeText"
+            class="mr-3"
+            depressed
+            small
+            color="primary"
+          >Associer à des éléments</v-btn>
+          <v-btn @click="removeText" depressed small color="error">Supprimer</v-btn>
+        </v-col>
+      </v-row>
       <v-row class="mt-8">
         <v-col cols="6" align="start">
           <h3 class="mb-4">Texte original</h3>
@@ -16,6 +28,9 @@
 </template>
 
 <script>
+import axios from "axios";
+import { TabsData } from "../../flux/Tabs";
+
 export default {
   props: {
     content: Object
@@ -26,6 +41,31 @@ export default {
     linkAnalyse: "",
     link: ""
   }),
+  methods: {
+    removeText() {
+      console.log(this.content.id);
+      console.log(this.content);
+      if (
+        confirm(
+          "Voulez-vous vraiment supprimer ce texte et ses analyses définitivement ?"
+        )
+      ) {
+        // Appel avec axios
+        let formData = new FormData();
+        formData.append("id", this.content.id);
+        axios
+          .post(process.env.VUE_APP_SERVEUR + "/supprimer-texte", formData)
+          .then(response => {
+            alert("Le texte a bien été supprimé");
+            TabsData.remove(TabsData.state.nowId);
+            console.log(response);
+          })
+          .catch(error => {
+            alert(error);
+          });
+      }
+    }
+  },
   mounted() {
     console.log(this.content);
     this.titre = this.content.titre;
